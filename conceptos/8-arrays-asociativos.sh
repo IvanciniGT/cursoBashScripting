bf12d7d1134aa1d66ff780e5a26d0ff489d81f97
Ubuntu <ubuntu@ip-172-31-6-144.eu-west-1.compute.internal>
2021-12-01 08:11:18 +0000

    asociativos

--- conceptos/8-arrays-asociativos.sh
+++ conceptos/8-arrays-asociativos.sh
@@ -12,9 +12,15 @@
 # En los arrays asociativos, los valores no van identificacos por su posicion, sino por una palabra
 # Es el equivalente en otros lenguajes de programación: MAPA o un DICCIONARIO
 # OPCION 1: 
-declare -A array=([clave1]=item1 [clave1]=item2 [clave1]=item3)
+declare -A array=([clave1]=item1 [clave2]=item2 [clave3]=item3)
 # OPCION 2: 
 declare -A array
 echo ${array["clave1"]}
 array["clave1"]="Otro valor"
 echo ${array["clave1"]}
+
+echo ${#array[@]}      # numero de elementos en el array
+for clave in ${!array[@]}
+do
+    echo $clave __ ${array[$clave]}
+done
---
+++ ejercicios/juegoPiedraPapelTijera.sh
@@ -0,0 +1,4 @@
+# Al que gane 2
+# Ordenador piensa que va a sacar
+# Preguntamos al jugador piedra papel o tijera?
+# Calculamos el ganador
