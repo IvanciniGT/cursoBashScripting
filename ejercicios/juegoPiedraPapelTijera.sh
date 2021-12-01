23dc17a917154ac3dc18fc03ef334a88806f6d37
Ubuntu <ubuntu@ip-172-31-6-144.eu-west-1.compute.internal>
2021-12-01 09:11:44 +0000

    asociativos

--- ejercicios/juegoPiedraPapelTijera.sh
+++ ejercicios/juegoPiedraPapelTijera.sh
@@ -1,4 +1,6 @@
-#!bin/bash
+#!/bin/bash
+. ../proyecto/aux/super_read.sh
+
 # Al que gane 2
 
 #                           JUGADOR
@@ -22,16 +24,31 @@ reglas[tijera,papel]=1
 reglas[tijera,tijera]=0
 
 opciones=( piedra papel tijera )
-# Ordenador piensa que va a sacar
-opcion_ordenador=$RANDOM%3  # 0 1 2
-opcion_ordenador=${opciones[$opcion_ordenador]}  # 0 1 2
-# Preguntamos al jugador piedra papel o tijera?
-super_read \
-    --prompt "Qué eliges" \
-    --error-message "Debes elegir 'piedra', 'papel' o 'tijera'"\
-    --retries 3 \
-    --allowed-values-list "piedra papel tijera" \
-    --question-mark "?" \
-    opcion_jugador
-# Calculamos el ganador
-ganador=${reglas[$opcion_jugador,$opcion_ordenador]}
+resultados=( 0 0 0 )
+mensaje_ganador=( "Hemos empatado" "Has ganado tu" "He ganado yo")
+
+#until (( ${resultados[1]} == 2 || ${resultados[2]} == 2 ))
+while (( ${resultados[1]} != 2 && ${resultados[2]} != 2 ))
+do
+    # Ordenador piensa que va a sacar
+    opcion_ordenador=$RANDOM%3  # 0 1 2
+    opcion_ordenador=${opciones[$opcion_ordenador]}  # 0 1 2
+    # Preguntamos al jugador piedra papel o tijera?
+    super_read \
+        --prompt "Qué eliges" \
+        --error-message "Debes elegir 'piedra', 'papel' o 'tijera'"\
+        --retries 3 \
+        --allowed-values-list "piedra papel tijera" \
+        --question-mark "?" \
+        opcion_jugador
+    # Calculamos el ganador
+    ganador=${reglas[$opcion_jugador,$opcion_ordenador]}
+    let resultados[$ganador]=${resultados[$ganador]}+1
+    # Mostrar el ganador de la mano
+    echo ${mensaje_ganador[ganador]}
+done
+
+echo HEMOS ACABADO
+ganador_final=$(( ${resultados[1]} == 2 ))   #  Si gana el jugador:   True   1
+                                             #  Si gana el ordenador: False  0
+echo $ganador_final                                         
