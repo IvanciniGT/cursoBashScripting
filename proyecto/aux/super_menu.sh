04350d45d77393173c5845562559c96f1e7a8b26
Ubuntu <ubuntu@ip-172-31-6-144.eu-west-1.compute.internal>
2021-12-01 11:29:55 +0000

    menu

--- proyecto/aux/super_menu.sh
+++ proyecto/aux/super_menu.sh
@@ -1,53 +1,53 @@
 #!/bin/bash
-##
-# Menu principal
-#  1. Administrar Servicios
-#  2. Monitorizar Servicios
-#
-#  0. Salir
 
-# Elija una opción [2]: 
-
-# funcion  Administrar servicios: echo "Estoy en AS"
-# funcion  Monitorizar servicios: echo "Estoy en MS"
 . $(dirname $BASH_SOURCE)/super_read.sh # Según POSIX
 
-function administrarServicios(){
-    echo "Estoy en administrar servicios"
-    pausa
-}
-function monitorizarServicios(){
-    echo "Estoy en monitorizar servicios"
-    pausa
-}
-function salir(){
-    echo "Gracias por utilizar el super monitorizador ACME®"
-    exit 0
-}
 
 function super_menu(){
-    while true
+    local fichero_menu=$1
+    
+    local titulo
+    local opcion
+    declare -a opciones_menu
+    declare -a funciones
+    
+    # Procesar el fichero del menu
+    while read linea
+    do
+        echo "$linea"
+        if [[ -z "$titulo" ]]; then
+            titulo=$linea
+        else
+            opcion=${linea#*=}
+            funcion=${linea%=*}
+            opciones_menu+=( "$opcion" )
+            funciones+=( "$funcion" )
+        fi
+    done < $fichero_menu.menu
+    
+    # Generar el menu
+    while [[ "$opcion" != "0" ]]
     do
         clear
-        titulo "Menú principal"
-        echo "   1. Administrar Servicios"
-        echo "   2. Monitorizar Servicios"
+        titulo "$titulo"
+        for ((i=1;i<${#opciones_menu[@]};i++))
+        do
+            echo "   $( amarillo $i ). ${opciones_menu[$i]}"
+        done
         echo
-        echo "   0. Salir"
+        echo "   $( azul 0 ). ${opciones_menu[0]}"
         azul $(linea)
         
         super_read \
             --prompt "Elija una opción" \
             --error-message "Debe escribir el número de la opción elegida" \
             --retries 3 \
-            --validation-pattern "^[0-2]$" \
+            --validation-pattern "^[0-${#opciones_menu[@]}]$" \
             --default 1 \
             --question-mark ":" \
             opcion
         
-        funciones=( salir administrarServicios monitorizarServicios )
         ${funciones[$opcion]}
     done
 }
 
-super_menu
---
+++ proyecto/monitorizador.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+. ./aux/super_menu.sh
+
+##
+# Menu principal
+#  1. Administrar Servicios
+#  2. Monitorizar Servicios
+#
+#  0. Salir
+
+# Elija una opción [2]: 
+
+function administrarServicios(){
+    super_menu "menus/administrar.servicios"
+}
+function monitorizarServicios(){
+    super_menu "menus/monitorizar.servicios"
+}
+function salir(){
+    clear
+    echo "Gracias por utilizar el super monitorizador ACME®"
+    exit 0
+}
+
+function volver(){
+    echo
+}
+function iniciarMonitorizacion(){
+    echo
+}
+function pararMonitorizacion(){
+    echo
+}
+function estadoServicios(){
+    echo
+}
+function altaServicio(){
+    echo
+}
+function bajaServicio(){
+    echo
+}
+
+
+
+super_menu "menus/principal"
